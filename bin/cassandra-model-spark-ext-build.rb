5bddb68696232f77643ca310775af386244ef700
Thomas Rogers <thomasrogers03@gmail.com>
2015-12-13 16:16:11 -0500

    Create build executable

---
+++ bin/cassandra-model-spark-ext-build.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+lib = File.expand_path(File.dirname(__FILE__) + '/../lib')
+$LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
+
+require 'cassandra_model_spark'
+require 'cassandra_model_spark/build'
+
