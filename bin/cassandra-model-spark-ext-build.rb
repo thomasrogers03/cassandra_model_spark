05c1e661a0eda401f19ea74fe5fccf579a9d480c
Thomas Rogers <thomasrogers03@gmail.com>
2015-12-13 16:21:15 -0500

    Require build script without the rest

--- bin/cassandra-model-spark-ext-build.rb
+++ bin/cassandra-model-spark-ext-build.rb
@@ -3,6 +3,5 @@
 lib = File.expand_path(File.dirname(__FILE__) + '/../lib')
 $LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
 
-require 'cassandra_model_spark'
 require 'cassandra_model_spark/build'
 
