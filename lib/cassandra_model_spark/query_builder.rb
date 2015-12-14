ed9e0f3cd83be09202c8c665f99b503836332db3
Thomas Rogers <thomasrogers03@gmail.com>
2015-12-14 17:58:32 -0500

    Implement QueryBuilder#as_data_frame

--- lib/cassandra_model_spark/query_builder.rb
+++ lib/cassandra_model_spark/query_builder.rb
@@ -1,7 +1,9 @@
 module CassandraModel
   class QueryBuilder
     def as_data_frame
-      Spark::DataFrame.new(@record_klass, nil)
+      updated_restriction = @record_klass.restriction_attributes(@params).stringify_keys.to_java
+      rdd = SparkCassandraHelper.filterRDD(@record_klass.rdd, updated_restriction)
+      Spark::DataFrame.new(@record_klass, rdd)
     end
   end
 end
--- spec/classes/query_builder_spec.rb
+++ spec/classes/query_builder_spec.rb
@@ -4,7 +4,17 @@ module CassandraModel
   describe QueryBuilder do
     let(:rdd) { double(:rdd) }
     let(:record_klass) { double(:klass, rdd: rdd) }
-    let(:query_builder) { QueryBuilder.new(record_klass) }
+    let(:restriction_key) { Faker::Lorem.word.to_sym }
+    let(:restriction_value) { Faker::Lorem.word }
+    let(:restriction) { {restriction_key => restriction_value} }
+    let(:java_restriction) { JavaHash[restriction.stringify_keys] }
+    let(:query_builder) { QueryBuilder.new(record_klass).where(restriction) }
+
+    before do
+      allow(record_klass).to receive(:restriction_attributes) do |restriction|
+        restriction
+      end
+    end
 
     describe '#as_data_frame' do
       let(:data_frame) { query_builder.as_data_frame }
@@ -20,7 +30,20 @@ module CassandraModel
       describe 'filtering the rdd' do
         subject { frame_rdd }
 
+        its(:rdd) { is_expected.to eq(rdd) }
+        its(:restriction) { is_expected.to eq(java_restriction) }
 
+        context 'when the record klass modifies the restriction for querying' do
+          let(:java_restriction) { JavaHash["rk_#{restriction_key}" => restriction_value] }
+
+          before do
+            allow(record_klass).to receive(:restriction_attributes) do |restriction|
+              restriction.inject({}) { |memo, (key, value)| memo.merge!(:"rk_#{key}" => value) }
+            end
+          end
+
+          its(:restriction) { is_expected.to eq(java_restriction) }
+        end
       end
     end
   end
