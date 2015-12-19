4a804540adc4baf48cfd1a55ba1d11dfbc219d0d
Thomas Rogers <thomasrogers03@gmail.com>
2015-12-19 16:51:59 -0500

    Wrap restriction columns in backticks

--- spec/classes/data_frame_spec.rb
+++ spec/classes/data_frame_spec.rb
@@ -303,20 +303,20 @@ module CassandraModel
 
         describe 'restricting the data set' do
           let(:restriction) { {partition: 47} }
-          let(:query_sql) { "SELECT * FROM #{table_name} WHERE partition = 47" }
+          let(:query_sql) { "SELECT * FROM #{table_name} WHERE `partition` = 47" }
 
           it { is_expected.to eq(query) }
 
           context 'with a multi-column restriction' do
             let(:restriction) { {partition: 30, clustering: 20.0} }
-            let(:query_sql) { "SELECT * FROM #{table_name} WHERE partition = 30 AND clustering = 20.0" }
+            let(:query_sql) { "SELECT * FROM #{table_name} WHERE `partition` = 30 AND `clustering` = 20.0" }
 
             it { is_expected.to eq(query) }
           end
 
           context 'when the restriction contains strings' do
             let(:restriction) { {partition: 'part'} }
-            let(:query_sql) { "SELECT * FROM #{table_name} WHERE partition = 'part'" }
+            let(:query_sql) { "SELECT * FROM #{table_name} WHERE `partition` = 'part'" }
 
             it { is_expected.to eq(query) }
           end
@@ -324,20 +324,20 @@ module CassandraModel
           context 'when the restriction contains a timestamp' do
             let(:time) { Time.now }
             let(:restriction) { {partition: time} }
-            let(:query_sql) { "SELECT * FROM #{table_name} WHERE partition = '#{time}'" }
+            let(:query_sql) { "SELECT * FROM #{table_name} WHERE `partition` = '#{time}'" }
 
             it { is_expected.to eq(query) }
           end
 
           context 'when the key is a KeyComparer' do
             let(:restriction) { {:price.gt => 50.49} }
-            let(:query_sql) { "SELECT * FROM #{table_name} WHERE price > 50.49" }
+            let(:query_sql) { "SELECT * FROM #{table_name} WHERE `price` > 50.49" }
 
             it { is_expected.to eq(query) }
           end
 
           context 'when the columns are mapped' do
-            let(:query_sql) { "SELECT * FROM #{table_name} WHERE ck_partition = 47" }
+            let(:query_sql) { "SELECT * FROM #{table_name} WHERE `ck_partition` = 47" }
 
             before do
               allow(record_klass).to(receive(:select_column)) { |column| :"ck_#{column}" }
