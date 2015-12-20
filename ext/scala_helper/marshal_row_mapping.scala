41d50f74fb7947768e21070a92d74368d9d33519
Thomas Rogers <thomasrogers03@gmail.com>
2015-12-20 17:14:56 -0500

    Create simple Ruby map/hash to SparkRow row mapping

--- ext/scala_helper/marshal_row_mapping.scala
+++ ext/scala_helper/marshal_row_mapping.scala
@@ -18,7 +18,46 @@ object MapStringStringRowMapping {
       val value = decoder.getValue()
 
       value match {
-        case (m: Map[AnyRef, AnyRef]) => m map { case (key, value) => (key.toString, value.toString) }
+        case (m: Map[_, _]) => m map { case (key, value) => (key.toString, value.toString) }
+        case _ => new IllegalArgumentException("Unsupported Ruby Type")
+      }
+    } else {
+      blob
+    }
+  }
+
+  private def updatedRow(row: CassandraRow): CassandraRow = {
+    val columns = row.columnNames
+    val values = row.columnValues.map{
+      value => value match {
+        case (blob: Array[Byte]) => decodeValue(blob)
+        case _ => value
+      }
+    }
+
+    new CassandraRow(columns, values)
+  }
+
+  def mappedRDD(rdd: RDD[CassandraRow]): RDD[CassandraRow] = {
+    rdd.map(
+      row => updatedRow(row)
+    )
+  }
+}
+
+object SparkRowRowMapping {
+  private def canDecode(blob: Array[Byte]) = {
+    new String(blob.slice(0, 4)) == "MRSH"
+  }
+
+  private def decodeValue(blob: Array[Byte]): AnyRef = {
+    if (canDecode(blob)) {
+      val decoder = new MarshalLoader(blob)
+      val value = decoder.getValue()
+
+      value match {
+        case (m: Map[_, _]) => Row.fromSeq(m.values.toSeq)
+        case (a: Array[_]) => Row.fromSeq(a.toSeq)
         case _ => new IllegalArgumentException("Unsupported Ruby Type")
       }
     } else {
