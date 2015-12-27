b8023a00c0262be82d052a7629d78238a140d010
Thomas Rogers <thomasrogers03@gmail.com>
2015-12-26 23:06:22 -0500

    Add scripts for spark master/slaves

---
+++ bin/cmodel-spark-slaves.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+lib = File.expand_path(File.dirname(__FILE__) + '/../lib')
+$LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
+
+require 'bundler/setup'
+require 'cassandra_model_spark'
+require 'cassandra_model_spark/launcher'
+
+command = ARGV.shift.downcase.to_sym
+launcher = CassandraModel::Spark::Launcher.new
+case command
+  when :start
+    launcher.start_slaves
+  when :stop
+    launcher.stop_slaves
+  else
+    puts '=> only supports start or stop'
+end
+
