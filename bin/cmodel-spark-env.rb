72328fb1cdedfa7ab12fb115affc4de1c35e8a17
Thomas Rogers <thomasrogers03@gmail.com>
2015-12-27 11:04:07 -0500

    Remove SPARK_CLASSPATH

--- bin/cmodel-spark-env.rb
+++ bin/cmodel-spark-env.rb
@@ -6,6 +6,6 @@ $LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
 require 'bundler/setup'
 require 'cassandra_model_spark/spark'
 
-print "export SPARK_HOME=#{CassandraModel::Spark.home} SPARK_CLASSPATH=#{CassandraModel::Spark.classpath}"
+print "export SPARK_HOME=#{CassandraModel::Spark.home}"
 
 
