4c67aff399761f333ebe9cd5cae4c541f029aebd
Thomas Rogers <thomasrogers03@gmail.com>
2015-12-27 10:38:49 -0500

    Add some environment helpers to make it easier to run spark master/slave in jruby

---
+++ bin/cmodel-spark-env.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+lib = File.expand_path(File.dirname(__FILE__) + '/../lib')
+$LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
+
+require 'bundler/setup'
+require 'cassandra_model_spark/spark'
+
+print "SPARK_HOME=#{CassandraModel::Spark.home} SPARK_CLASSPATH=#{CassandraModel::Spark.classpath}"
+
+
--- lib/cassandra_model_spark/launcher.rb
+++ lib/cassandra_model_spark/launcher.rb
@@ -10,6 +10,8 @@ module CassandraModel
 
       def run_master
         raise NotImplementedError unless RUBY_ENGINE == 'jruby'
+        validate_env!
+
         SparkMaster.main(to_argv(run_master_args))
       end
 
@@ -21,6 +23,8 @@ module CassandraModel
 
       def run_slave
         raise NotImplementedError unless RUBY_ENGINE == 'jruby'
+        validate_env!
+
         SparkWorker.main(to_argv(run_slave_args))
       end
 
@@ -40,11 +44,18 @@ module CassandraModel
         args.split.to_java_argv
       end
 
+      def validate_env!
+        unless ENV['SPARK_HOME'] && File.expand_path(ENV['SPARK_HOME']) == Spark.home
+          raise 'Spark enviroment not set correctly'
+        end
+      end
+
       def add_master_jars
         ConnectionCache[nil].tap do |connection|
           connection.config = {spark: {master: master_url}}
           connection.spark_context.addJar("#{Spark.classpath}/cmodel_scala_helper.jar")
         end
+        ConnectionCache.clear
       end
 
       def workers
@@ -105,11 +116,15 @@ module CassandraModel
       end
 
       def env
-        @env ||= {
+        @env ||= spark_env.merge(ENV.to_hash)
+      end
+
+      def spark_env
+        @spark_env ||= {
             'SPARK_HOME' => Spark.home,
             'SPARK_CLASSPATH' => Spark.classpath,
             'SPARK_JARS' => Dir["#{Spark.classpath}/*.jar"] * ',',
-        }.merge(ENV.to_hash)
+        }
       end
 
     end
