070397671ab074a51670a7e383781ab6459a8124
Thomas Rogers <thomasrogers03@gmail.com>
2016-02-17 08:30:45 -0500

    Pass around RDD schema in order to ensure consistency. Add ability to transform the schema using the LuaRDD interface

--- ext/scala_helper/lua_rdd.scala
+++ ext/scala_helper/lua_rdd.scala
@@ -7,9 +7,9 @@ import org.luaj.vm2.lib.OneArgFunction
 import org.luaj.vm2.lib.VarArgFunction
 import org.luaj.vm2.LuaTable
 import org.apache.spark.rdd._
-import org.apache.spark.sql.Row
+import org.apache.spark.sql._
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.catalyst.expressions.GenericRow
 import java.io._
 
 class LuaRDDLib(val env: LuaValue) extends OneArgFunction {
@@ -22,10 +22,9 @@ class LuaRDDLib(val env: LuaValue) extends OneArgFunction {
 }
 
 object LuaRowValue {
-  def fromLuaTable(table: LuaTable): Row = {
+  def luaTableToRow(table: LuaTable): Row = {
     val keys = table.keys()
     val length = keys.length
-    val schema = new Array[StructField](length)
     val row = new Array[Any](length)
     var index = 0
 
@@ -36,21 +35,14 @@ object LuaRowValue {
         case num: LuaInteger => num.toint()
         case fnum: LuaDouble => fnum.tofloat()
       }
-      schema(index) = StructField(table_key.toString(), value match {
-        case str: LuaString => StringType
-        case num: LuaInteger => IntegerType
-        case fnum: LuaDouble => FloatType
-      })
       index += 1
     }
 
-    return new GenericRowWithSchema(row, StructType(schema))
+    return new GenericRow(row)
   }
 }
 
-class LuaRowValue(val row: Row) extends LuaValue {
-  val schema = row.schema
-
+class LuaRowValue(val schema: StructType, val row: Row) extends LuaValue {
   def `type`(): Int = 999
   def typename(): String = "Row"
 
@@ -72,60 +64,93 @@ class LuaRowValue(val row: Row) extends LuaValue {
   }
 }
 
-object LuaRDD {
-
-  def callScript(lua_code: String, row: Row): LuaValue = {
-    val globals = new Globals()
-    LuaC.install(globals)
-    globals.set("ROW", new LuaRowValue(row))
-
-    val chunk = globals.load(lua_code)
-    return chunk.call()
+class LuaRDD (val schema: StructType, val rdd: RDD[Row]) extends Serializable {
+  def map(new_schema: StructType, lua_code: String): LuaRDD = {
+    val new_rdd = rdd.map(callMapScript(lua_code, _))
+    new LuaRDD(new_schema, new_rdd)
   }
 
-  def callMapScript(lua_code: String, row: Row): Row = {
-    return callScript(lua_code, row) match {
-      case row: LuaRowValue => row.row
-      case table: LuaTable => LuaRowValue.fromLuaTable(table)
+  def filter(lua_code: String): LuaRDD = {
+    val new_rdd = rdd.filter(callFilterScript(lua_code, _))
+    new LuaRDD(schema, new_rdd)
+  }
+
+  def groupByString(lua_code: String): LuaRDD = {
+    val new_schema = groupBySchema(StringType)
+    val new_rdd = rdd.groupBy(callGroupByStringScript(lua_code, _))
+    val grouped_rdd = groupedRDD(new_rdd)
+
+    new LuaRDD(new_schema, grouped_rdd)
+  }
+
+  def groupByInt(lua_code: String): LuaRDD = {
+    val new_schema = groupBySchema(IntegerType)
+    val new_rdd = rdd.groupBy(callGroupByIntScript(lua_code, _))
+    val grouped_rdd = groupedRDD(new_rdd)
+
+    new LuaRDD(new_schema, grouped_rdd)
+  }
+
+  def groupByFloat(lua_code: String): LuaRDD = {
+    val new_schema = groupBySchema(FloatType)
+    val new_rdd = rdd.groupBy(callGroupByFloatScript(lua_code, _))
+    val grouped_rdd = groupedRDD(new_rdd)
+
+    new LuaRDD(new_schema, grouped_rdd)
+  }
+
+  def toDF(sql_context: SQLContext) = sql_context.createDataFrame(rdd, schema)
+
+  private def groupBySchema(data_type: DataType): StructType = {
+    val fields = Array(StructField("key", data_type), StructField("values", schema))
+    StructType(fields)
+  }
+
+  private def groupedRDD[T](rdd: RDD[(T, Iterable[Row])]): RDD[Row] = {
+    rdd.map { case (key, values) =>
+      val row: Array[Any] = Array(key, values)
+      new GenericRow(row)
     }
   }
 
-  def callFilterScript(lua_code: String, row: Row): Boolean = {
-    return callScript(lua_code, row) match {
+  private def callScript(lua_code: String, row: Row): LuaValue = {
+    val globals = new Globals()
+    LuaC.install(globals)
+    globals.set("ROW", new LuaRowValue(schema, row))
+
+    val chunk = globals.load(lua_code)
+    chunk.call()
+  }
+
+  private def callMapScript(lua_code: String, row: Row): Row = {
+    callScript(lua_code, row) match {
+      case row: LuaRowValue => row.row
+      case table: LuaTable => LuaRowValue.luaTableToRow(table)
+    }
+  }
+
+  private def callFilterScript(lua_code: String, row: Row): Boolean = {
+    callScript(lua_code, row) match {
       case bool: LuaBoolean => bool.toboolean()
     }
   }
 
-  def callGroupByStringScript(lua_code: String, row: Row): String = {
-    return callScript(lua_code, row) match {
+  private def callGroupByStringScript(lua_code: String, row: Row): String = {
+    callScript(lua_code, row) match {
       case str: LuaString => str.toString()
     }
   }
 
-  def callGroupByIntScript(lua_code: String, row: Row): Int = {
-    return callScript(lua_code, row) match {
+  private def callGroupByIntScript(lua_code: String, row: Row): Int = {
+    callScript(lua_code, row) match {
       case num: LuaInteger => num.toint()
     }
   }
 
-  def callGroupByFloatScript(lua_code: String, row: Row): Float = {
-    return callScript(lua_code, row) match {
+  private def callGroupByFloatScript(lua_code: String, row: Row): Float = {
+    callScript(lua_code, row) match {
       case fnum: LuaDouble => fnum.tofloat()
     }
   }
 
-}
-
-class LuaRDD (var rdd: RDD[Row]) {
-  def map(lua_code: String) = {
-    new LuaRDD(rdd.map(LuaRDD.callMapScript(lua_code, _)))
-  }
-
-  def filter(lua_code: String) = {
-    new LuaRDD(rdd.filter(LuaRDD.callFilterScript(lua_code, _)))
-  }
-
-  def groupByString(lua_code: String) = {
-    rdd.groupBy(LuaRDD.callGroupByStringScript(lua_code, _))
-  }
 }
