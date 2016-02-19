4726beae827751cf897a11d11e5c5ed9c6eae481
Thomas Rogers <thomasrogers03@gmail.com>
2016-02-18 21:31:33 -0500

    Pass around compiled lua byte code instead of the original code string

--- ext/scala_helper/lua_rdd.scala
+++ ext/scala_helper/lua_rdd.scala
@@ -2,6 +2,7 @@ package org.apache.spark.api.cassandra_model
 
 import org.luaj.vm2.Globals
 import org.luaj.vm2.compiler.LuaC
+import org.luaj.vm2.compiler.DumpState
 import org.luaj.vm2._
 import org.luaj.vm2.lib.jse.JseBaseLib
 import org.luaj.vm2.lib._
@@ -10,6 +11,8 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.catalyst.expressions.GenericRow
 import scala.reflect.ClassTag
+import java.io._
+import java.security.MessageDigest
 
 class LuaRDDLib(val env: LuaValue) extends OneArgFunction {
   override def call(arg: LuaValue): LuaValue = {
@@ -120,12 +123,14 @@ class PartitionableStringArray(val items: Array[String]) extends Serializable{
 
 object LuaRDD {
   private val thread_local_globals = new ThreadLocal[Globals]
+  private val digest = MessageDigest.getInstance("SHA-1")
 
   def getGlobals(): Globals = thread_local_globals.get()
   def newGlobals(): Globals = {
     val globals = new Globals()
 
     LuaC.install(globals)
+    LoadState.install(globals)
     globals.load(new JseBaseLib())
     globals.load(new PackageLib())
     globals.load(new TableLib())
@@ -140,30 +145,41 @@ object LuaRDD {
       globals = newGlobals()
     globals
   }
+
+  def getLuaCodeDigest(lua_code: String) = {
+    val hash_bytes = digest.digest(lua_code.getBytes())
+    new String(hash_bytes)
+  }
 }
 
 class LuaRDD (val schema: StructType, val rdd: RDD[Row]) extends Serializable {
+  private class LuaMetaData(val name: String, val byte_code: Array[Byte]) extends Serializable
+
   def map(new_schema: StructType, lua_code: String): LuaRDD = {
-    val new_rdd = rdd.map(callMapScript(lua_code, _))
+    val lua_byte_code = getLuaByteCode(lua_code)
+    val new_rdd = rdd.map(callMapScript(lua_byte_code, _))
     new LuaRDD(new_schema, new_rdd)
   }
 
   def filter(lua_code: String): LuaRDD = {
-    val new_rdd = rdd.filter(callFilterScript(lua_code, _))
+    val lua_byte_code = getLuaByteCode(lua_code)
+    val new_rdd = rdd.filter(callFilterScript(lua_byte_code, _))
     new LuaRDD(schema, new_rdd)
   }
 
   def groupByString(lua_code: String): LuaRDD = {
+    val lua_byte_code = getLuaByteCode(lua_code)
     val new_schema = groupBySchema(StringType)
-    val new_rdd = rdd.groupBy(callGroupByStringScript(lua_code, _))
+    val new_rdd = rdd.groupBy(callGroupByStringScript(lua_byte_code, _))
     val grouped_rdd = groupedRDD(new_rdd)
 
     new LuaRDD(new_schema, grouped_rdd)
   }
 
   def groupByStringArray(lua_code: String): LuaRDD = {
+    val lua_byte_code = getLuaByteCode(lua_code)
     val new_schema = groupBySchema(ArrayType(StringType))
-    val pre_rdd = rdd.groupBy(callGroupByStringArrayScript(lua_code, _))
+    val pre_rdd = rdd.groupBy(callGroupByStringArrayScript(lua_byte_code, _))
     val new_rdd: RDD[(Array[String], Iterable[Row])] = pre_rdd.map { case(key, values) =>
       (key.items, values)
     }
@@ -173,22 +189,34 @@ class LuaRDD (val schema: StructType, val rdd: RDD[Row]) extends Serializable {
   }
 
   def groupByInt(lua_code: String): LuaRDD = {
+    val lua_byte_code = getLuaByteCode(lua_code)
     val new_schema = groupBySchema(IntegerType)
-    val new_rdd = rdd.groupBy(callGroupByIntScript(lua_code, _))
+    val new_rdd = rdd.groupBy(callGroupByIntScript(lua_byte_code, _))
     val grouped_rdd = groupedRDD(new_rdd)
 
     new LuaRDD(new_schema, grouped_rdd)
   }
 
   def groupByFloat(lua_code: String): LuaRDD = {
+    val lua_byte_code = getLuaByteCode(lua_code)
     val new_schema = groupBySchema(FloatType)
-    val new_rdd = rdd.groupBy(callGroupByFloatScript(lua_code, _))
+    val new_rdd = rdd.groupBy(callGroupByFloatScript(lua_byte_code, _))
     val grouped_rdd = groupedRDD(new_rdd)
 
     new LuaRDD(new_schema, grouped_rdd)
   }
 
   def toDF(sql_context: SQLContext) = sql_context.createDataFrame(rdd, schema)
+
+  private def getLuaByteCode(lua_code: String) = {
+    val output_stream = new ByteArrayOutputStream()
+    val name = LuaRDD.getLuaCodeDigest(lua_code)
+    val prototype = LuaC.instance.compile(new ByteArrayInputStream(lua_code.getBytes()), name)
+    val success = DumpState.dump(prototype, output_stream, true)
+
+    output_stream.flush()
+    success match { case 0 => new LuaMetaData(name, output_stream.toByteArray()) }
+  }
 
   private def groupBySchema(data_type: DataType): StructType = {
     val fields = Array(StructField("key", data_type), StructField("values", ArrayType(schema)))
@@ -202,47 +230,48 @@ class LuaRDD (val schema: StructType, val rdd: RDD[Row]) extends Serializable {
     }
   }
 
-  private def callScript(lua_code: String, row: Row): LuaValue = {
+  private def callScript(lua_byte_code: LuaMetaData, row: Row): LuaValue = {
     val globals = LuaRDD.getGlobalsOrNew()
     globals.set("ROW", new LuaRowValue(schema, row))
 
-    val chunk = globals.load(lua_code)
+    val prototype = globals.loadPrototype(new ByteArrayInputStream(lua_byte_code.byte_code), lua_byte_code.name, "b")
+    val chunk = new LuaClosure(prototype, globals)
     chunk.call()
   }
 
-  private def callMapScript(lua_code: String, row: Row): Row = {
-    callScript(lua_code, row) match {
+  private def callMapScript(lua_byte_code: LuaMetaData, row: Row): Row = {
+    callScript(lua_byte_code, row) match {
       case row: LuaRowValue => row.row
       case table: LuaTable => LuaRowValue.luaTableToRow(table)
     }
   }
 
-  private def callFilterScript(lua_code: String, row: Row): Boolean = {
-    callScript(lua_code, row) match {
+  private def callFilterScript(lua_byte_code: LuaMetaData, row: Row): Boolean = {
+    callScript(lua_byte_code, row) match {
       case bool: LuaBoolean => bool.toboolean()
     }
   }
 
-  private def callGroupByStringScript(lua_code: String, row: Row): String = {
-    callScript(lua_code, row) match {
+  private def callGroupByStringScript(lua_byte_code: LuaMetaData, row: Row): String = {
+    callScript(lua_byte_code, row) match {
       case str: LuaString => str.toString()
     }
   }
 
-  private def callGroupByStringArrayScript(lua_code: String, row: Row): PartitionableStringArray = {
-    callScript(lua_code, row) match {
+  private def callGroupByStringArrayScript(lua_byte_code: LuaMetaData, row: Row): PartitionableStringArray = {
+    callScript(lua_byte_code, row) match {
       case table: LuaTable => new PartitionableStringArray(LuaRowValue.luaTableToArray(table))
     }
   }
 
-  private def callGroupByIntScript(lua_code: String, row: Row): Int = {
-    callScript(lua_code, row) match {
+  private def callGroupByIntScript(lua_byte_code: LuaMetaData, row: Row): Int = {
+    callScript(lua_byte_code, row) match {
       case num: LuaInteger => num.toint()
     }
   }
 
-  private def callGroupByFloatScript(lua_code: String, row: Row): Float = {
-    callScript(lua_code, row) match {
+  private def callGroupByFloatScript(lua_byte_code: LuaMetaData, row: Row): Float = {
+    callScript(lua_byte_code, row) match {
       case fnum: LuaDouble => fnum.tofloat()
     }
   }
