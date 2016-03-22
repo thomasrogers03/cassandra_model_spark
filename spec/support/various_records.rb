c19567bf05dd63e1c73e3e2fc3c5fd98b926f6c6
thomasrogers03 <thomasrogers03@gmail.com>
2016-03-22 08:51:04 -0400

    Work towards replacing DataFrame mock records with real ones

--- spec/support/various_records.rb
+++ spec/support/various_records.rb
@@ -1,9 +1,16 @@
 module VariousRecords
   extend RSpec::SharedContext
 
+  let(:extra_basic_record_klass) do
+    create_query = 'CREATE TABLE IF NOT EXISTS super_basic (key text, value text, description text, extra_data blob, PRIMARY KEY (key, value))'
+    CassandraModel::ConnectionCache[nil].session.execute(create_query)
+    Class.new(CassandraModel::Record) do
+      self.table_name = :super_basic
+    end
+  end
   let(:basic_record_klass) do
     Class.new(CassandraModel::Record) do
-      name = :"basic_#{Faker::Lorem.word}"
+      name = :basic
       partition_key = {key: :text}
       clustering_columns = {value: :text}
       fields = {description: :text, extra_data: :blob}
@@ -19,13 +26,24 @@ module VariousRecords
   let(:composite_record_klass) do
     Class.new(CassandraModel::Record) do
       extend CassandraModel::DataModelling
-      self.table_name = :"composite_#{Faker::Lorem.word}"
+      self.table_name = :composite
       model_data do |inquirer, data_set|
         inquirer.knows_about(:key)
         data_set.is_defined_by(:value)
         data_set.knows_about(:description, :extra_data)
         data_set.change_type_of(:extra_data).to(:blob)
       end
+    end
+  end
+
+  def generate_simple_model(name, partition, clustering, fields)
+    column_types = (partition + clustering + fields).map { |key| "#{key} text" } * ', '
+    partition = "(#{partition * ', '})"
+    clustering = "#{clustering * ', '}"
+    create_query = "CREATE TABLE IF NOT EXISTS #{name} (#{column_types}, PRIMARY KEY (#{partition}, #{clustering}))"
+    CassandraModel::ConnectionCache[nil].session.execute(create_query)
+    Class.new(CassandraModel::Record) do
+      self.table_name = name
     end
   end
 
