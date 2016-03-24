f79be09d047b4a82a98b75e49e4cbe7d7da621f8
thomasrogers03 <thomasrogers03@gmail.com>
2016-03-24 07:57:50 -0400

    Clean up ColumnDeserializer

--- ext/scala_helper/column_deserializer.scala
+++ ext/scala_helper/column_deserializer.scala
@@ -8,23 +8,24 @@ import com.datastax.spark.connector.rdd._
 import org.apache.spark.sql.types._
 
 object ColumnDeserializer {
-  private def decodeValue(blob: Array[Byte]): AnyRef = {
-    val decoder = new MarshalLoader(blob)
-    decoder.getValue()
+  def mappedRDD(rdd: RDD[CassandraRow], column: Int): RDD[CassandraRow] = {
+    rdd.map(updatedRow(_, column))
   }
 
   private def updatedRow(row: CassandraRow, column: Int): CassandraRow =
   {
     val columns = row.columnNames
-    val updated_value = row.columnValues(column) match {
-      case (blob: Array[Byte]) => decodeValue(blob)
-    }
+    val updated_value = getDecodedValue(row, column)
     val values = row.columnValues.updated(column, updated_value)
 
     new CassandraRow(columns, values)
   }
 
-  def mappedRDD(rdd: RDD[CassandraRow], column: Int): RDD[CassandraRow] = {
-    rdd.map(updatedRow(_, column))
+  private def getDecodedValue(row: CassandraRow, column: Int): AnyRef = row.columnValues(column) match {
+    case (blob: Array[Byte]) => decodeValue(blob)
+  }
+
+  private def decodeValue(blob: Array[Byte]): AnyRef = {
+    new MarshalLoader(blob).getValue()
   }
 }
