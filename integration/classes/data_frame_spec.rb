7af3a17a15da45a2bc9b0165a8804536beb8a4a7
thomasrogers03 <thomasrogers03@gmail.com>
2016-03-28 18:49:57 -0400

    Add some more testing for DataFrame integration spec

--- integration/classes/data_frame_spec.rb
+++ integration/classes/data_frame_spec.rb
@@ -1,13 +1,12 @@
 require 'integration_spec_helper'
 
 describe 'querying a DataFrame' do
-  let(:record_klass) { composite_record_klass }
+  let(:record_klass) { generate_composite_model(:frame_test, {key: :text}, {value: :text}, {description: :text}) }
   let!(:record) do
     record_klass.create(
         key: Faker::Lorem.word,
         value: Faker::Lorem.sentence,
-        description: Faker::Lorem.paragraph,
-        extra_data: nil
+        description: Faker::Lorem.paragraph
     )
   end
 
@@ -16,6 +15,10 @@ describe 'querying a DataFrame' do
 
     it 'should allow us to query the original record' do
       expect(data_frame.first).to eq(record)
+    end
+
+    it 'should allow us to query for column sub-sets' do
+      expect(data_frame.select(:value, :description).first).to eq(record_klass.new(value: record.value, description: record.description))
     end
   end
 end
