3eb5f03e313f925810c58c68b897f1d0dfa94047
Thomas Rogers <thomasrogers03@gmail.com>
2016-05-17 21:29:24 -0400

    Create a method to prepare for migrating data from one Record class to another

--- ext/scala_helper/cassandra_helper.scala
+++ ext/scala_helper/cassandra_helper.scala
@@ -3,12 +3,19 @@ package org.apache.spark.api.cassandra_model
 import org.apache.spark._
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.rdd._
+import com.datastax.spark.connector.cql._
 import java.util._
 import scala.collection.JavaConversions.mapAsScalaMap
 
 object CassandraHelper {
   def cassandraTable(sc: SparkContext, keyspace: String, table: String) = {
     sc.cassandraTable(keyspace, table)
+  }
+
+  def saveRDDToCassandra(sc: SparkContext, rdd: CassandraRDD[CassandraRow], keyspace: String, table: String, host: String) = {
+    implicit val c = CassandraConnector(sc.getConf.set("spark.cassandra.connection.host", host))
+
+    rdd.saveToCassandra(keyspace, table)
   }
 
   def filterRDD(rdd: CassandraRDD[CassandraRow], restriction: HashMap[String, Any]) = {
