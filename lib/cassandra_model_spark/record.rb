6fb4d9e65b6c10ffae93d3f64a5f277b44d62b27
thomasrogers03 <thomasrogers03@gmail.com>
2016-05-26 08:10:41 -0400

    Use Spark.application.spark_context instead of connection spark context

--- lib/cassandra_model_spark/record.rb
+++ lib/cassandra_model_spark/record.rb
@@ -2,7 +2,7 @@ module CassandraModel
   class Record
     def self.rdd
       @spark_rdd ||= Spark::Lib::SparkCassandraHelper.cassandraTableForHost(
-          table.connection.spark_context,
+          Spark.application.spark_context,
           table.connection.config[:keyspace],
           table_name,
           table.connection.config[:hosts].first)
