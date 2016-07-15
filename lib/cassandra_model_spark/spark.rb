71359abdf610cb69676d7e2025e117d525b8d7ed
thomasrogers03 <thomasrogers03@gmail.com>
2016-07-15 08:00:27 -0400

    Add support for local spark configurations

--- lib/cassandra_model_spark/spark.rb
+++ lib/cassandra_model_spark/spark.rb
@@ -8,16 +8,24 @@ module CassandraModel
         @gem_root ||= File.expand_path('../../..', __FILE__)
       end
 
+      def application_root
+        @app_root ||= File.expand_path('./')
+      end
+
       def home
         @home ||= (ENV['SPARK_HOME'] || default_home)
       end
 
       def config_path
-        @config_path ||= "#{home}/config"
+        @config_path ||= if File.exists?(spark_config_path(application_root))
+                           "#{application_root}/config"
+                         else
+                           "#{home}/config"
+                         end
       end
 
       def config_file_path
-        @config_file_path ||= "#{config_path}/spark.yml"
+        @config_file_path ||= spark_config_path(config_path)
       end
 
       def config
@@ -44,6 +52,10 @@ module CassandraModel
       end
 
       private
+
+      def spark_config_path(directory)
+        "#{directory}/config/spark.yml"
+      end
 
       def default_classpath
         File.expand_path('./lib/', home).tap do |path|
